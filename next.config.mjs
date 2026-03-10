09413b71bb6d4d3612336dfd5a44caa26dc023dd
Abishek1919 <abishekrajen@example.com>
2026-03-10 18:12:32 +0530

    products image update

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,8 @@ const nextConfig = {
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
-      { protocol: "https", hostname: "m.media-amazon.com" }
+      { protocol: "https", hostname: "m.media-amazon.com" },
+      { protocol: "https", hostname: "www.cetaphil.com" }
     ]
   }
 };
